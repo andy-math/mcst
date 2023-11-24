6f87aad54b3724c78816012397ceb5f9756862cd
Andy <72276976+andy-math@users.noreply.github.com>
2023-11-24 14:05:57 +0800

    change test directory

--- configure.m
+++ configure.m
@@ -1,4 +1,13 @@
-function [testdir, pydir] = configure()
-    testdir = "test";
-    pydir = "py";
+function [mdir, pydir] = configure()
+    if ~isfolder('test.m')
+        mkdir('test.m');
+    end
+    if ~isfolder('test.m/m')
+        mkdir('test.m/m');
+    end
+    if ~isfolder('test.m/py')
+        mkdir('test.m/py');
+    end
+    mdir = 'test.m/m';
+    pydir = 'test.m/py';
 end
