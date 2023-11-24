35ce338066ba40a715e45f966f4ba1ca2e71f458
Andy <72276976+andy-math@users.noreply.github.com>
2023-11-24 14:18:32 +0800

    Update MCSTTest.m

--- MCSTTest.m
+++ MCSTTest.m
@@ -11,28 +11,28 @@ classdef MCSTTest < matlab.unittest.TestCase
     end
     methods (Test)
         function testMainM(self)
-            self.verifyEqual(MCSTTest.readFile('main.m'), MCSTTest.readFile('test/main.m'));
+            self.verifyEqual(MCSTTest.readFile('main.m'), MCSTTest.readFile('test_m/m/main.m'));
         end
         function testOutputM(self)
-            self.verifyEqual(MCSTTest.readFile('output.m'), MCSTTest.readFile('test/output.m'));
+            self.verifyEqual(MCSTTest.readFile('output.m'), MCSTTest.readFile('test_m/m/output.m'));
         end
         function testMainM2(self)
-            self.verifyEqual(MCSTTest.readFile('test/main.m'), MCSTTest.readFile('test2/main.m'));
+            self.verifyEqual(MCSTTest.readFile('test_m/m/main.m'), MCSTTest.readFile('test_py/m/main.m'));
         end
         function testOutputM2(self)
-            self.verifyEqual(MCSTTest.readFile('test/output.m'), MCSTTest.readFile('test2/output.m'));
+            self.verifyEqual(MCSTTest.readFile('test_m/m/output.m'), MCSTTest.readFile('test_py/m/output.m'));
         end
         function testMainPy(self)
-            self.verifyEqual(MCSTTest.readFile('py/main.py'), MCSTTest.readFile('py2/main.py'));
+            self.verifyEqual(MCSTTest.readFile('test_m/py/main.py'), MCSTTest.readFile('test_py/py/main.py'));
         end
         function testOutputPy(self)
-            self.verifyEqual(MCSTTest.readFile('py/output.py'), MCSTTest.readFile('py2/output.py'));
+            self.verifyEqual(MCSTTest.readFile('test_m/py/output.py'), MCSTTest.readFile('test_py/py/output.py'));
         end
         function testNodesM(self)
             files = dir('mcst');
             for i = 1:numel(files)
                 if ~(startsWith(files(i).name, '.') || endsWith(files(i).name, '.asv'))
-                    self.verifyEqual(MCSTTest.readFile(['mcst/', files(i).name]), MCSTTest.readFile(['test/', files(i).name]));
+                    self.verifyEqual(MCSTTest.readFile(['mcst/', files(i).name]), MCSTTest.readFile(['test_m/m/', files(i).name]));
                 end
             end
         end
@@ -40,8 +40,8 @@ classdef MCSTTest < matlab.unittest.TestCase
             files = dir('mcst');
             for i = 1:numel(files)
                 if ~(startsWith(files(i).name, '.') || endsWith(files(i).name, '.asv'))
-                    self.verifyEqual(MCSTTest.readFile(['py/nodes/', files(i).name(1:end-2), '.py']), ...
-                        MCSTTest.readFile(['py2/nodes/', files(i).name(1:end-2), '.py']));
+                    self.verifyEqual(MCSTTest.readFile(['test_m/py/nodes/', files(i).name(1:end-2), '.py']), ...
+                        MCSTTest.readFile(['test_py/py/nodes/', files(i).name(1:end-2), '.py']));
                 end
             end
         end
